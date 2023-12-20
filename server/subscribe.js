4d53e98710fec0c2ad9e4f485b474b8af3fc3452
Minh Nghĩa <109430898+minhnghia1103@users.noreply.github.com>
2023-12-20 22:16:59 +0700

    hello

--- server/subscribe.js
+++ server/subscribe.js
@@ -1,4 +1,6 @@
 const mqtt = require("mqtt");
+const socketIO = require("socket.io");
+const http = require("http");
 
 // Thông tin kết nối đến MQTT broker
 const brokerUrl = "mqtt://192.168.1.3"; // Địa chỉ IP của máy chủ MQTT
@@ -6,6 +8,31 @@ const topic = "esp32/data";
 
 // Kết nối đến broker
 const client = mqtt.connect(brokerUrl, { protocol: "mqtt" }); // Chú ý: Thêm protocol vào đây
+
+// Tạo máy chủ HTTP và kết nối socket.io
+const server = http.createServer();
+const io = socketIO(server);
+
+// Khi có kết nối từ client
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Khi nhận được tin nhắn từ MQTT
+  client.on("message", (mqttTopic, message) => {
+    if (mqttTopic === topic) {
+      const payload = message.toString();
+      console.log(`Received message from ${mqttTopic}: ${payload}`);
+
+      // Gửi tin nhắn đến client thông qua socket.io
+      socket.emit("mqtt_message", payload);
+    }
+  });
+
+  // Khi ngắt kết nối từ client
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 // Khi kết nối thành công
 client.on("connect", () => {
@@ -15,13 +42,13 @@ client.on("connect", () => {
   client.subscribe(topic);
 });
 
-// Khi nhận được một tin nhắn từ chủ đề đã đăng ký
-client.on("message", (topic, message) => {
-  console.log(`Received message from ${topic}: ${message.toString()}`);
-  // Xử lý tin nhắn ở đây
-});
-
 // Xử lý lỗi nếu có
 client.on("error", (err) => {
   console.error(`Error: ${err}`);
+});
+
+// Lắng nghe trên cổng 3000 (hoặc cổng khác nếu bạn muốn)
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
